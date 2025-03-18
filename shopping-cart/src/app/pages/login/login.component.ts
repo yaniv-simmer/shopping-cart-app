4fde711a2364decbf36b56112d89c3abae6dbb7f
yaniv-simmer <simmeryaniv@gmail.com>
2025-03-18 23:59:29 +0200

    added password validators

--- shopping-cart/src/app/pages/login/login.component.ts
+++ shopping-cart/src/app/pages/login/login.component.ts
@@ -15,7 +15,7 @@ export class LoginComponent {
   constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
