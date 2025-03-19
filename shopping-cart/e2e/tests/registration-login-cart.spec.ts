3ec7d7974615c5cc7ee5826dccbd128778a0dc83
yaniv-simmer <simmeryaniv@gmail.com>
2025-03-19 22:43:01 +0200

    remove async

--- shopping-cart/e2e/tests/registration-login-cart.spec.ts
+++ shopping-cart/e2e/tests/registration-login-cart.spec.ts
@@ -4,48 +4,40 @@ const email = 'test@example.com';
 const password = 'Password1';
 const URL = 'http://localhost:4200';
 
-test('User Registration', async ({ page }) => {
+test('User Flow', async ({ page }) => {
+  // User Registration
   await page.goto(URL + '/register');
   await page.fill('input[formControlName="email"]', email);
   await page.fill('input[formControlName="password"]', password);
   await page.fill('input[formControlName="confirmPassword"]', password);
   await page.click('button[type="submit"]');
   await expect(page).toHaveURL(URL + '/login');
-});
 
-test('AuthGuard', async ({ page }) => {
+  // AuthGuard
   await page.goto(URL + '/app/products');
   await expect(page).toHaveURL(URL + '/login');
-});
 
-test('User Login', async ({ page }) => {
-  await page.goto(URL + '/login');
-  await page.fill('input[formControlName="email"]', email);
-  await page.fill('input[formControlName="password"]', password);
-  await page.click('button[type="submit"]');
-  await expect(page).toHaveURL(URL + '/app/products');
-});
-
-test('Add Product to Cart', async ({ page }) => {
+  // User Login
   await page.goto(URL + '/login');
   await page.fill('input[formControlName="email"]', email);
   await page.fill('input[formControlName="password"]', password);
   await page.click('button[type="submit"]');
   await expect(page).toHaveURL(URL + '/app/products');
 
+  // Add Product to Cart
   await page.click('a[routerLink="/app/products"]');
   await page.click('button:has-text("Add")');
   await page.click('a[routerLink="/app/cart"]');
-  await expect(page.locator('.cart-item')).toHaveCount(1);
+  await expect(page.locator('.product-card')).toHaveCount(1);
 
-  // log out and then log in again to check if the cart still has the product
+  // Log out and then log in again to check if the cart still has the product
   await page.click('button:has-text("Logout")');
   await page.goto(URL + '/login');
   await page.fill('input[formControlName="email"]', email);
   await page.fill('input[formControlName="password"]', password);
   await page.click('button[type="submit"]');
-  await expect(page).toHaveURL(URL + '/app/products');
+  await expect(page).toHaveURL(URL + '/app/await page.locator('div').filter({ hasText: /^Ferrari hat\$14\.99Add$/ }).getByRole('button').click();products');
 
   await page.click('a[routerLink="/app/cart"]');
-  await expect(page.locator('.cart-item')).toHaveCount(1);
+  await expect(page.locator('.product-card')).toHaveCount(1);
 });
